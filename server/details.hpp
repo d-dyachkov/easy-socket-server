0919cada91a391762d53826799d5b2ccf1dd150d
Denis Dyachkov <denis.dyachkov@softomate.com>
2020-02-17 10:28:44 +0700

    update func sign in details

--- server/details.hpp
+++ server/details.hpp
@@ -5,15 +5,15 @@
 #include <netinet/in.h>
 namespace easy_socket::details {
 template<typename T>
-T get_sockaddr_by_socket_descriptor(int socket_descriptor) {
+T get_sockaddr_by_socket(int socket) {
   T addr{};
   socklen_t len = sizeof(addr);
-  getsockname(socket_descriptor, (sockaddr *) &addr, &len);
+  getsockname(socket, (sockaddr *) &addr, &len);
   return addr;
 }
 
 inline uint16_t get_port_by_socket(int socket) {
-  auto addr = get_sockaddr_by_socket_descriptor<sockaddr_in>(socket);
+  auto addr = get_sockaddr_by_socket<sockaddr_in>(socket);
   return ntohs(addr.sin_port);
 }
 }
